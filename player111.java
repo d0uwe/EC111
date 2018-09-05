efcf218f203777f847d80cf1186831470534087f
Pieter <p.kronemeijer@xs4all.nl>
2018-09-05 15:20:29 +0200

    tabs -> spaces

--- player111.java
+++ player111.java
@@ -8,48 +8,48 @@ import java.util.ArrayList;
 
 public class player111 implements ContestSubmission
 {
-	Random rnd_;
-	ContestEvaluation evaluation_;
+    Random rnd_;
+    ContestEvaluation evaluation_;
     private int evaluations_limit_;
-	
-	public player111()
-	{
-		rnd_ = new Random();
-	}
-	
-	public void setSeed(long seed)
-	{
-		// Set seed of algortihms random process
-		rnd_.setSeed(seed);
-	}
 
-	public void setEvaluation(ContestEvaluation evaluation)
-	{
-		// Set evaluation problem used in the run
-		evaluation_ = evaluation;
-		
-		// Get evaluation properties
-		Properties props = evaluation.getProperties();
+    public player111()
+    {
+        rnd_ = new Random();
+    }
+
+    public void setSeed(long seed)
+    {
+        // Set seed of algortihms random process
+        rnd_.setSeed(seed);
+    }
+
+    public void setEvaluation(ContestEvaluation evaluation)
+    {
+        // Set evaluation problem used in the run
+        evaluation_ = evaluation;
+
+        // Get evaluation properties
+        Properties props = evaluation.getProperties();
         // Get evaluation limit
         evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
-		// Property keys depend on specific evaluation
-		// E.g. double param = Double.parseDouble(props.getProperty("property_name"));
+        // Property keys depend on specific evaluation
+        // E.g. double param = Double.parseDouble(props.getProperty("property_name"));
         boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
         boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
         boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
 
-		// Do sth with property values, e.g. specify relevant settings of your algorithm
+        // Do sth with property values, e.g. specify relevant settings of your algorithm
         if(isMultimodal){
             // Do sth
         }else{
             // Do sth else
         }
     }
-    
-	public void run()
-	{
-		// Run your algorithm here
-        
+
+    public void run()
+    {
+        // Run your algorithm here
+
         int evals = 0;
         int pop_size = 150;
         // init po1pulation
@@ -71,5 +71,5 @@ public class player111 implements ContestSubmission
             // Select survivors
         }
 
-	}
+    }
 }
